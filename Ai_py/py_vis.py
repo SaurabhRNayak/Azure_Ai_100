fd6a40620ef54ff9b9eb6bf8758c69a447d05466
Saurabh_R_Nayak <46881166+SaurabhRNayak@users.noreply.github.com>
2021-01-21 09:17:37 +0000

    Update py_vis.py

--- Ai_py/py_vis.py
+++ Ai_py/py_vis.py
@@ -15,7 +15,8 @@ def store(container_name,img_path,key,connect_str,acc_name):
     blob_service_client = BlobServiceClient.from_connection_string(connect_str)
     # container_name = container_name + str(uuid.uuid4())
     container = ContainerClient.from_connection_string(connect_str, container_name)
-
+    
+    # Container check whether it already exists
     try:
         container_properties = container.get_container_properties()
         container_client = ContainerClient.from_connection_string(conn_str=connect_str, container_name=container_name)
@@ -29,12 +30,15 @@ def store(container_name,img_path,key,connect_str,acc_name):
     # block_blob_service = BlockBlobService(account_name=acc_name,account_key=key)
     blob_name=os.path.basename(img_path)
     # block_blob_service.create_blob_from_path(container_client, blob_name, img_path)
+    
+    # Blob check if the blob already exists
     blob_client = container_client.get_blob_client(blob_name)
     if not (blob_client.exists()):
         with open(img_path, "rb") as data:
             blob_client.upload_blob(data, blob_type="BlockBlob")
-        pass
+    
 
+# Vision analyze api usage
 def analyze(key,img,path):
     headers = {'Ocp-Apim-Subscription-Key': key,'Content-Type':'application/octet-stream'}
     params = {'visualFeatures': 'Categories,Description,Color'}
@@ -44,8 +48,11 @@ def analyze(key,img,path):
     response.raise_for_status()
     analysis = response.json()
     json_value=json.dumps(response.json())
-    print(json.dumps(response.json()))
-    image_caption = analysis["description"]["captions"][0]["text"].capitalize()
+    # Gives the complete response
+    print(json.dumps(response.json())) ## working on module to save the data in cosmosdb
+#     image_caption = analysis["description"]["captions"][0]["text"].capitalize()
+#     print(image_caption)
+
 
 if __name__ == "__main__":
     subs_key=config_.config['Cognitive Services API key']
